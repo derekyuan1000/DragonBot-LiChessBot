7a88574f89f8500cf7c35619f79dd65442615cfa
derekyuan1000 <derekyuan10000@gmail.com>
2024-12-18 17:41:37 +0700

    hosting

--- engines/bot/main.py
+++ engines/bot/main.py
@@ -37,5 +37,28 @@ def get_move(board, depth):
                 top_move = move
                 top_eval = eval
 
-    print("CHOSEN MOVE: ", top_move, "WITH EVAL: ", top_eval)
+    print("CHOSEN MOVE (FOR CURRENT SIDE): ", top_move, "WITH EVAL: ", top_eval)
+
+    # Simulate the opponent's perspective
+    board.push(top_move)
+    opponent_best_move = None
+    opponent_top_eval = -np.inf if board.turn == chess.WHITE else np.inf
+
+    for move in board.legal_moves:
+        board.push(move)
+        eval = minimax(board, depth - 1, -np.inf, np.inf, board.turn)
+        board.pop()
+
+        if board.turn == chess.WHITE:
+            if eval > opponent_top_eval:
+                opponent_best_move = move
+                opponent_top_eval = eval
+        else:
+            if eval < opponent_top_eval:
+                opponent_best_move = move
+                opponent_top_eval = eval
+
+    board.pop()
+
+    print("OPPONENT'S PERSPECTIVE (BEST MOVE): ", opponent_best_move, "WITH EVAL: ", opponent_top_eval)
     return top_move
